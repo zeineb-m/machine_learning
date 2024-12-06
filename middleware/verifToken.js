600bfb190b72e06623da1e4100e2974a96863591
Ben said Youssef <youssefbensaid827@gmail.com>
2024-12-06 16:25:20 +0100

    add new function to vefify admin auth

--- middleware/verifToken.js
+++ middleware/verifToken.js
@@ -14,4 +14,24 @@ export const verify = (req, res, next) =>{
       return res.status(401).json("You are not authenticated!");
     }
   }
+
+const isAdmin = (token) => {
+const arrayToken = token.split(".");
+const tokenPlayload = JSON.parse(atob(arrayToken[1]))
+return tokenPlayload.isAdmin ;
+}
+
+export const verifyAdmin = (req, res, next) => {
+    const authHeader = req.headers.token;
+    if (authHeader) {
+        const token = authHeader.split(" ")[1];
+        if (isAdmin(token)) {
+            next();
+        } else {
+            return res.status(403).json("You are not authorized to perform this action!");
+    
+        }
+    }
+}
   
+
--- routes/User.js
+++ routes/User.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 const router = Router();
 import { getAllUsers , getUserById , deleteUserById} from '../controllers/User.js';
-import { verify } from '../middleware/verifToken.js';
+import { verify , verifyAdmin } from '../middleware/verifToken.js';
 
-router.route('/').get(verify , getAllUsers);
+router.route('/').get(verifyAdmin ,verify , getAllUsers);
 router.route('/:id').get(verify, getUserById);
 router.route('/:id').delete(verify , deleteUserById);
 
 export default router;
+
