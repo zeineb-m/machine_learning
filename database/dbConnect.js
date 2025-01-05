10782da0e6ee07f5c65f53a1eb4645eaf0741646
Ben said Youssef <youssefbensaid827@gmail.com>
2025-01-05 23:46:17 +0100

    delete Message file from controller and model

--- database/dbConnect.js
+++ database/dbConnect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDatabase = async ()=>{
 try{
-    const {connection} = await mongoose.connect("mongodb+srv://aa:aa@cluster0.d2hao.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    const {connection} = await mongoose.connect("mongodb://localhost:27017/testExam")
     console.log(`MongoDB connected : ${connection.host}`)
 }catch(error){
     console.log(error)
--- models/User.js
+++ models/User.js
@@ -2,10 +2,12 @@ import mongoose from "mongoose";
 
 
 const userSchema = new mongoose.Schema({
-      username :{
-        type : String , 
-        required:true
-       },
+      username :{ 
+         type: String,
+         required: true,
+         minlength: 3,
+         maxlength: 30 
+        },
        email : {
         type : String , 
         required : true , 
