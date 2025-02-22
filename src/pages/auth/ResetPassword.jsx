0565498c3d3a5a3acdaf950fea73a50771842273
mohamedbsila <101298975+mohamedbsila@users.noreply.github.com>
2025-02-22 16:58:25 +0100

    forget password ^-^

--- src/pages/auth/ResetPassword.jsx
+++ src/pages/auth/ResetPassword.jsx
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import './LoginSignup.css'; // Import the same CSS for styling
-
+import './LoginSignup.css'; 
 const ResetPassword = () => {
   const location = useLocation();
   const navigate = useNavigate();
